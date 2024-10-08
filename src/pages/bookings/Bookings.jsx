0ffd88af4f749d9ab360eefe0857bf6930a6c682
codeftsaikat <soikatdey18@gmail.com>
2024-10-08 09:00:07 -0700

    Initialize delete section on the booking part

--- src/pages/bookings/Bookings.jsx
+++ src/pages/bookings/Bookings.jsx
@@ -5,8 +5,26 @@ import BookingRow from "./BookingRow";
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
   const { user } = useContext(AuthContext);
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Deleted Successfully");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
 
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
@@ -18,8 +36,8 @@ const Bookings = () => {
 
   return (
     <div>
-      <h2 className="text-5xl text-center">
-        Your Bookings: 0{bookings.length}
+      <h2 className="text-5xl text-center mb-8">
+        {bookings.length < 10 ? `0${bookings.length}` : bookings.length}
       </h2>
       <div className="overflow-x-auto">
         <table className="table">
@@ -31,17 +49,22 @@ const Bookings = () => {
                   <input type="checkbox" className="checkbox" />
                 </label>
               </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-           {
-            bookings.map((booking)=><BookingRow key={booking._id} booking={booking}/>)
-           }
+            {bookings.map((booking) => (
+              <BookingRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+              />
+            ))}
           </tbody>
         </table>
       </div>
